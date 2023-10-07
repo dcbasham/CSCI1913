47f6d8094ff69f9200b46d3a428a6c127e013964
dcbasham <dauriebasham@gmail.com>
2023-10-06 21:15:46 -0500

    all good

--- lab04/pricebook.py
+++ lab04/pricebook.py
@@ -1,4 +1,4 @@
-#Daurie Basham
+# Daurie Basham
 def is_sorted(pricebook):
     """takes a list of tuples and checks if it is
     sorted ascending by name (string), which is the
@@ -29,6 +29,7 @@ def price_average(pricebook):
     else:
         return sum_pb / items
 
+
 def unsorted_get(pricebook, name):
     """Search pricebook by name and return its price. If name not found,
     return None
@@ -37,6 +38,7 @@ def unsorted_get(pricebook, name):
         if pricebook[i][1] == name:
             return pricebook[i][0]
     return None
+
 
 def unsorted_put(pricebook, name, price):
     """pricebook is an unsorted list of tuples (name, price).
@@ -56,13 +58,13 @@ def unsorted_put(pricebook, name, price):
             elif i == len(pricebook) - 1:
                 pricebook.append(new_tuple)
 
+
 def sorted_get(pricebook, name):
     """Search pricebook by name and return its price. If name not found,
     return None
      O(N)"""
     high = len(pricebook) - 1
     low = 0
-
     if len(pricebook) >= 1:
         while high >= low:
             mid = (low + high) // 2
@@ -75,28 +77,23 @@ def sorted_get(pricebook, name):
     else:
         return None
 
+
 def sorted_put(pricebook, name, price):
     """takes a sorted pricebook (list of tuples). each tuple is 2 elements:
-    a float digit price and string name. Update pricebook to reflect the new price
-    and keep sorted order"""
-    new_tuple = (price, name)
-    last = len(pricebook) - 1
-    low = len(pricebook) - 1
-    high = 0
-    count = 0
+    a price and name. Update pricebook to reflect the new price
+    and keep in sorted order"""
+    j = len(pricebook) - 1
     if len(pricebook) >= 1:
-        while high <= low:
-            mid = (low + high) // 2
-            if pricebook[mid][1] == name:
-                pricebook.insert(count, new_tuple)
-                pricebook.pop(count + 1)
-                print('done')
-                return pricebook
-            elif pricebook[mid][1] < name:
-                low = mid - 1
-                count += 1
-            elif pricebook[mid][1] > name:
-                high = mid + 1
-                count += 1
+        for i in range(len(pricebook)):
+            if name == pricebook[i][1]:
+                pricebook.pop(i)
+                pricebook.insert(i, (price, name))
+                break
+            elif name < pricebook[i][1]:
+                pricebook.insert(i, (price, name))
+                break
+            elif i == j:
+                pricebook.append((price, name))
+                break
     else:
-        pricebook.append(new_tuple)
+        pricebook.append((price, name))
